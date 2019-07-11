699205d1a67f1c51e0ef87023a4259e849b35178
LenaVeselova <lena.veselova88@gmail.com>
2019-07-11 15:16:43 +0300

    'quotes'

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,42 +1,42 @@
-const form = document.getElementById("mainForm");
-form.addEventListener("submit", e => {
+const form = document.getElementById('mainForm');
+form.addEventListener('submit', e => {
     e.preventDefault();
 
-    let nameEl = document.querySelector('input[name="name"]');
-    nameEl.classList.remove("error");
+    let nameEl = document.querySelector("input[name='name']");
+    nameEl.classList.remove('error');
 
     let name = nameEl.value;
 
     if (name.length < 5) {
-        nameEl.classList.add("error");
+        nameEl.classList.add('error');
         nameEl.insertAdjacentHTML(
-            "afterend",
-            "<span style='color:#A60000; margin-top:0.2rem; font-size:.8rem'>Наименование должно содержать не менее 5 символов</span>"
+            'afterend',
+            '<span>Наименование должно содержать не менее 5 символов</span>'
         );
     }
 
-    let catEl = document.querySelector('select[name="category"]');
-    catEl.classList.remove("error");
+    let catEl = document.querySelector("select[name='category']");
+    catEl.classList.remove('error');
 
     let category = catEl.value;
 
     if (Number(category) < 1) {
-        catEl.classList.add("error");
+        catEl.classList.add('error');
         catEl.insertAdjacentHTML(
-            "afterend",
-            "<span style='color:#A60000; margin-top:0.2rem; font-size:.8rem'>Пожалуйста, выберите категорию</span>"
+            'afterend',
+            '<span>Пожалуйста, выберите категорию</span>'
         );
     }
 
-    let descEl = document.querySelector('textarea[name="description"]');
-    descEl.classList.remove("error");
+    let descEl = document.querySelector("textarea[name='description']");
+    descEl.classList.remove('error');
 
     let description = descEl.value;
     if (description.length < 10) {
-        descEl.classList.add("error");
+        descEl.classList.add('error');
         descEl.insertAdjacentHTML(
-            "afterend",
-            "<span style='color:#A60000; margin-top:0.2rem; font-size:.8rem'>Описание должно содержать не менее 10 символов</span>"
+            'afterend',
+            '<span>Описание должно содержать не менее 10 символов</span>'
         );
     }
 });
