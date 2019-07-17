b51356f31820afe1ace6b9f73825beee7357a576
LenaVeselova <lena.veselova88@gmail.com>
2019-07-17 14:02:24 +0300

    ...

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,14 +9,14 @@ const showError = (el, text) => {
 const clearError = el => {
     el.classList.remove('error');
 
-    let parentEl = el.parentNode;
-    let textEl = parentEl.querySelector('.error-text');
+    let parentEl = el.parentNode; //parentNode - это свойство элемента, который передаётся в функцию в качестве аргумента el
 
-    if (textEl !== null) {
-        parentEl.removeChild(textEl);
+    let textEl = parentEl.querySelector('.error-text'); //querySelector здесь не как метод document вызываться, а как метод найденного "родителя". '.error-text' это селектор, по которому можно найти span с текстом ошибки. В кавычках, потому что это строка
+    if (textEl !== null) { //если не ставить проверку if, будет ошибка, т.к. при первом вызове ещё нет текста с ошибкой и textEl просто отсутствует, а если его нет, то и удалять нечего
+
+        parentEl.removeChild(textEl); //Родитель удаляет потомка
     }
 };
-
 
 const form = document.getElementById('mainForm');
 form.addEventListener('submit', e => {
@@ -48,4 +48,16 @@ form.addEventListener('submit', e => {
     if (description.length < 10) {
         showError(descEl, 'Описание должно содержать не менее 10 символов')
     }
+});
+
+const clearEl = form.querySelector("button[type='reset']");
+clearEl.addEventListener('click', e => {
+    let x = form.querySelectorAll('.error');
+    clearError();
+});
+
+form.querySelector('button[type="reset"]').addEventListener('click', () => {
+    form.querySelectorAll('.error').forEach(el => {
+        clearError(el);
+    });
 });
